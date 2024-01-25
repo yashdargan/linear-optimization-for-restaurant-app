d0692fd803616e9839db65f7292671bd6589331b
rohan8998 <rohanmanchanda09@gmail.com>
2024-01-25 23:45:58 +0530

    Employee-Scheduling:basic mechanism

--- Src/data_processing.py
+++ Src/data_processing.py
@@ -11,15 +11,19 @@ def preprocessing(df):
 
     #df['delivery_time'] = (df['actual_delivery_time'] - df['created_at']).dt.total_seconds() / 60
     #df['hour'] = (df['created_at'].dt.hour) + 1 
-    #df['day'] = (df['created_at'].dt.dayofweek) + 1
-    #df['shift'] = df['created_at'].apply(categorize_shift)
+    #Find the earlist date
+    min_date = df['created_at'].min()
+    df['day'] = (df['created_at'] - min_date).dt.days + 1
+    df['shift'] = df['created_at'].apply(categorize_shift)
+    df_day100 = df[df['day']==100]
+    st.write(df_day100)
     #df = df.drop(columns=['market_id','created_at','actual_delivery_time','store_primary_category','order_protocol','total_item','subtotal','num_distinct_items','min_item_price','max_item_price'])
-    store_partner = df.groupby(['store_id'])['total_onshift_partners'].max().reset_index()
+    store_partner = df.groupby(['store_id','shift'])['total_onshift_partners'].max().reset_index()
     store_partner.dropna(subset=['total_onshift_partners'],inplace=True)
     order_store = df.groupby(['store_id'])['total_items'].sum().reset_index()
     result = {
         'hourly_partner': store_partner,
-        'data': df,
+        'data': df_day100,
         'order_store': order_store,
         'total_day': total_days,
      }
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 from Src.data_loading import load_file
+from Src.optimization import optimization
+from Src.data_processing import preprocessing
+from Src.EDA import EDA
 def main():
     st.title("Streamlit Data Loading Example")
 
     file_path = 'Data/dataset.csv'
     df= load_file(file_path)
-    st.write(df.head(50)) 
-
+    st.write(df.head(50))
+    #EDA(df)
+    df1=preprocessing(df)
+    optimization(df1) 
+   
 if __name__ == "__main__":
     main()
